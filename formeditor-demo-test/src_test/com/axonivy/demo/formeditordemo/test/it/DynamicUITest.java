da68df0267961610ebedc9287b0b045bed4edc6d
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-12-05 10:57:02 +0700

    handle feedback

--- formeditor-demo-test/src_test/com/axonivy/demo/formeditordemo/test/it/DynamicUITest.java
+++ formeditor-demo-test/src_test/com/axonivy/demo/formeditordemo/test/it/DynamicUITest.java
@@ -2,21 +2,20 @@ package com.axonivy.demo.formeditordemo.test.it;
 
 import org.junit.jupiter.api.Test;
 
-import com.axonivy.demo.formeditordemo.enums.Country;
 import com.axonivy.ivy.webtest.IvyWebTest;
 
 @IvyWebTest
 public class DynamicUITest extends WebBaseSetup {
 
-  private static final String COUNTRY_DROPDOWN_CSS_SELECTOR = "#form\\:country";
-  private static final String FEDERAL_STATE_DROPDOWN_CSS_SELECTOR = "#form\\:federal-state";
+  private static final String COUNTRY_DROPDOWN_CSS_SELECTOR = "#form:country";
+  private static final String FEDERAL_STATE_DROPDOWN_CSS_SELECTOR = "#form:federal-state";
 
   @Test
   void dynamicUIShowFederalStateWhenCountryOfGermanyIsChosen() {
     login();
     startDynamicUIProcess();
     // Click to "Germany" option in Country drop-down
-    verifyAndClickItemLabelInDropdown(COUNTRY_DROPDOWN_CSS_SELECTOR, Country.GERMANY.getName(), DROPDOWN_LIST_SUFFIX, DROPDOWN_LABEL_SUFFIX);
+    verifyAndClickItemLabelInDropdown(COUNTRY_DROPDOWN_CSS_SELECTOR, "Germany", DROPDOWN_LIST_SUFFIX, DROPDOWN_LABEL_SUFFIX);
     // Check if "Federal State" is rendered
     verifyElementVisible(FEDERAL_STATE_DROPDOWN_CSS_SELECTOR);
   }
